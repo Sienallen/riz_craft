27ece00c13ab468ad3b6e621eedec0713f52bd21
Allen Sien <allensien25@gmail.com>
2025-06-29 22:22:29 -0700

    isAuth created for checking authentication

--- Frontend/src/Components/Dropdown/DropDownProfile.tsx
+++ Frontend/src/Components/Dropdown/DropDownProfile.tsx
@@ -15,56 +15,13 @@ interface prop {
 const DropDownProfile = ({ setDropDown, dropDown }: prop) => {
   const [isAuth, setIsAuth] = useState<boolean | null>(null);
 
-  /*  useEffect(() => {
+  useEffect(() => {
     const checkAuth = async () => {
       const result = await isAuthenticated();
       setIsAuth(result);
     };
     checkAuth();
-  }, [dropDown]); */
-
-  useEffect(() => {
-    auth().catch(() => setIsAuth(false));
   }, [dropDown]);
-
-  const refreshToken = async () => {
-    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-    try {
-      const res = await AxiosInstance.post('/api/token/refresh/', {
-        refresh: refreshToken,
-      });
-      if (res.status === 200) {
-        localStorage.setItem(ACCESS_TOKEN, res.data.access);
-        setIsAuth(true);
-      } else {
-        setIsAuth(false);
-      }
-    } catch (error) {
-      console.log(error);
-      setIsAuth(false);
-    }
-  };
-
-  const auth = async () => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    if (!token) {
-      setIsAuth(false);
-      return;
-    }
-    const decoded = jwtDecode(token);
-    const tokenExpiration = decoded.exp;
-    const now = Date.now() / 1000;
-
-    if (tokenExpiration === undefined) {
-      throw new Error('tokenExpiration is undefined');
-    }
-
-    if (tokenExpiration < now) {
-      await refreshToken();
-    } else {
-      setIsAuth(true);
-    }
-  };
 
   const name = !isAuth ? 'Sign in' : 'Sign out';
 
